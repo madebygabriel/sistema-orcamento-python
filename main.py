ae5375240ebd56b0277f6b9fadce47ba5960f1d8
Gabriel Busch <gabrielsoftware.dev@gmail.com>
2026-03-22 13:06:09 -0300

    feat:  listagem de produtos, relatório do orçamento, e opção para salvar em .json

--- main.py
+++ main.py
@@ -5,16 +5,17 @@ class Client:
         self.email = email
         self._budgets = []
 
+    # Adiciona o orçamento na lista "self._budgets"
     def add_budget(self, budget):
         self._budgets.append(budget)
 
 
 class Product:
-    def __init__(self, code, name, quantity, unit_price):
+    def __init__(self, code, name, quantity, price):
         self.code = code
         self.name = name
         self.quantity = quantity
-        self.price = unit_price
+        self.price = price
 
 
 class Budget:
@@ -30,37 +31,76 @@ class Budget:
     @client.setter
     def client(self, client):
         self._client = client
+
+        # Add budget to the Client class
         client.add_budget(self)
 
-    def add_products(self, product):
+    # Adiciona produtos a lista de _products
+    def add_product(self, product):
+        """
+        product.total_price = total price in the multiplication of price and quantity
+            - right after adding the products to the list "self._products"
+        """
+        product.total_price = product.price * product.quantity
         self._products.append(product)
 
+    # Listagem de produtos
     def list_products(self):
-        total = 0
+        """
+        Each product that goes through "for" is converted into a dunder dict.
+        """
+
         return [
-            {
-                'name': product.name,
-                'quantity': product.quantity,
-                'price': product.price,
-                'total_price': product.price * product.quantity,
-            }
+            product.__dict__
             for product in self._products
         ]
 
-    def calc_total_price(self):
-        total = 0
-        for product in self._products:
-            total += product.price * product.quantity
-        
-        return total
+    # Preço total do orçamento
+    def total_price_budget(self):
+        """
+        self.cost_price = variable that will store the value;
+        self._products = class variable that will send items to: product;
+        returning the final value.
+        """
 
+        self.cost_price = 0
+
+        for product in self._products:
+            self.cost_price += product.price * product.quantity
+
+        return self.cost_price
+
+    # Criação de relatório do orçamento (todas informações)
+    def budget_report(self):
+        # Report inicialization (dict)
+        report = {'client': {}, 'products': {}}
+
+        # Client informations
+        report['client']['name'] = self.client.name
+        report['client']['telephone'] = self.client.telephone
+        report['client']['email'] = self.client.email
+
+        # Products informations
+        report['products'] = self.list_products()
+
+        # Total price budget 
+        report['price_budget'] = self.total_price_budget()
+
+        return report
+    
+    # Relatório para salvar em .json
+    def save_report_json(self):
+        import json
+
+        with open('report_budget.json', 'w', encoding='utf-8') as file:
+            json.dump(self.budget_report(), file, ensure_ascii=False, indent=4)
 
 # PRODUTOS
-quadro_carreta = Product(code='Q1M3', name='Quadro Carreta', quantity=2, unit_price=7056.97)
-paredes = Product(code='P1M3', name='Parede Quadricular', quantity=2, unit_price=6090.55)
-eletrica = Product(code='EE45', name='Parte Elétrica Total', quantity=30, unit_price=104.90)
-fixacao = Product(code='P4I51', name='Acessórios de Fixação', quantity=200, unit_price=1.94)
-ferramentas = Product(code='AF4PO5', name='Discos, acessórios e bronze', quantity=78, unit_price=78.00)
+quadro_carreta = Product(code='Q1M3', name='Quadro Carreta', quantity=2, price=7056.97)
+paredes = Product(code='P1M3', name='Parede Quadricular', quantity=2, price=6090.55)
+eletrica = Product(code='EE45', name='Parte Elétrica Total', quantity=30, price=104.90)
+fixacao = Product(code='P4I51', name='Acessórios de Fixação', quantity=200, price=1.94)
+ferramentas = Product(code='AF4PO5', name='Discos, acessórios e bronze', quantity=78, price=78.00)
 
 # CLIENTE
 c1 = Client('Rodrigo Rocha', '99887-7665', 'rodrigo@gmail.com')
@@ -70,5 +110,6 @@ orcamento1 = Budget(1)
 orcamento1.client = c1
 orcamento1.add_products(quadro_carreta)
 orcamento1.add_products(paredes)
-print(orcamento1.list_products())
+
+orcamento1.save_report_json()
 
